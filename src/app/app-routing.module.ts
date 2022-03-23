7f0ca071242e455750da49d16df92ad709a88ba4
Ahmed Mansour <fs.amansour@gmail.com>
2022-03-23 23:13:04 +0200

    Theming and Landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
