243493dfc7c6e272ba34e9fb0a843de5f5204f3c
Ahmed Mansour <fs.amansour@gmail.com>
2022-04-08 09:16:25 +0200

    remove build files from git

--- docs/common.44406da2e5694b16.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkAI_Maturity_Assessment=self.webpackChunkAI_Maturity_Assessment||[]).push([[592],{5101:(o,_,t)=>{t.d(_,{C:()=>c,Z:()=>a});var u=t(2382);class a extends u.Oe{constructor(s,e,r){super(s,e,r)}}class c extends u.cw{constructor(s,e,r){super(s,e,r)}}}}]);
